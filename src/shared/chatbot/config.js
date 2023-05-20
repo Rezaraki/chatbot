a380e6066778807b8a4808832cc68c82ad321d65
Rezaraki <rezakadil@gmail.com>
2023-05-21 00:07:04 +0430

    not needed files deleted

--- src/shared/chatbot/config.js
+++
@@ -1,7 +0,0 @@
-import { createChatBotMessage } from 'react-chatbot-kit';
-
-const config = {
-    initialMessages: [createChatBotMessage(`say hello to me`)],
-};
-
-export default config;
--- src/shared/utils.ts
+++
@@ -1,2 +0,0 @@
-export const makeEmptyArrs=(num:number):null[]=> new Array(num).fill(null)
-export const DATE_FORMAT="YYYY-MM-DD" 
